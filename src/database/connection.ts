15e2d388affd480b125613515252c545f7b7632f
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-22 21:35:25 +0545

    user table migration

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
 
-const sequelize = new Sequelize(envConfig.connectionString as string)
-
+const sequelize = new Sequelize(envConfig.connectionString as string,{
+    models : [__dirname + '/models']
+})
 
 //check 
 try{

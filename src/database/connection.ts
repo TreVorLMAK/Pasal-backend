0bb6890226f449b6a24566f3a511cf1d8845bdb6
TreVorLMAK <bimalbhandari563@gmail.com>
2024-12-22 21:30:15 +0545

    user table

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -17,4 +17,8 @@ try{
                 console.log(error)
                 }
 
+                sequelize.sync({force : false}).then(()=>{
+                    console.log("local changes injected to database successfully")
+                })
+
 export default sequelize
